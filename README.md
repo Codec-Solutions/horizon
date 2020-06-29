8e834a1054e54637b19c3f6727de53b42bffbefe
Roland Edi <lambirou225@gmail.com>
2020-06-29 16:31:08 +0000

    Commit 29-06-2020

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-# Horizon framework
+<h1 align="center">Horizom Framework</h1>
 
-PHP framework
+<p align="center">
+<a href="https://packagist.org/packages/horizom/horizom"><img src="https://poser.pugx.org/horizom/horizom/d/total.svg" alt="Total Downloads"></a>
+<a href="https://packagist.org/packages/horizom/horizom"><img src="https://poser.pugx.org/horizom/horizom/v/stable.svg" alt="Latest Stable Version"></a>
+<a href="https://packagist.org/packages/horizom/horizom"><img src="https://poser.pugx.org/horizom/horizom/license.svg" alt="License"></a>
+</p>
 
-## Changelog
+## About Horizom
 
-### Commit 05-05-2020
+Horizom is a light web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling.
 
-- [x] Routing complet basé sur [AuraRouter](http://auraphp.com/packages/3.x/Router)
-- [x] Moteur de template simple et puissant [Blade Template de Laravel](https://laravel.com/docs/5.8/blade)
-- [ ] Système d'injection de dépendance [PHP-DI](http://php-di.org/) (en cours...)
-- [ ] Documentation de 80% du code source...
+## Learning Horizom
 
-### Commit 04-05-2020
+Horizom has the most simple documentation of any modern web application framework. The [Horizom documentation](https://horizom.github.io/framework) is in-depth and complete, making it a breeze to get started learning the framework.
 
-- [x] Ajout d'un Dispatcher psr15
-- [x] Système de routing
+## License
+
+The Horizom framework is open-sourced software licensed under the [MIT license](LICENSE.md).
--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Horizom - Light PHP Framework
+ *
+ * @package  Horizom
+ * @author   Horizom Team <horizom-team@gmail.com>
+ */
+
 if (version_compare(PHP_VERSION, '7.2.5') === -1) {
     $version = explode('-', PHP_VERSION);
     echo 'This version of Horizom requires at least PHP 7.2.5. ';
@@ -7,18 +14,45 @@ if (version_compare(PHP_VERSION, '7.2.5') === -1) {
     return;
 }
 
-define('ROOT', dirname(dirname(__FILE__)));
-define('WEBROOT', '/public/');
-define('PUBLIC', ROOT . WEBROOT);
-define('APP', ROOT . '/app/');
-define('CONFIG', ROOT . '/config/');
-define('RESOURCES', ROOT . '/resources/');
-
 /**
  * Framawork version
  */
 define('HORIZOM_VERSION', '1.0.0');
 
-require ROOT . '/vendor/autoload.php';
+define('HORIZOM_START', microtime(true));
+define('HORIZOM_ROOT', dirname(dirname(__FILE__)));
+define('HORIZOM_WEBROOT', '/public/');
+define('HORIZOM_PUBLIC', ROOT . WEBROOT);
+define('HORIZOM_APP', ROOT . '/app/');
+define('HORIZOM_CONFIG', ROOT . '/config/');
+define('HORIZOM_RESOURCES', ROOT . '/resources/');
 
-(new \Horizom\Core\App)->run();
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
+|
+*/
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$app = new Horizom\Core\App();
+$app->run();
